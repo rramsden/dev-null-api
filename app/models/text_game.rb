afacc75ef3b2ef899cfc232df2787ab8d70ad6cb
Richard Ramsden <richard@rramsden.ca>
2015-04-07 17:13:35 +0900

    Add text-game class

---
+++ app/models/text_game.rb
@@ -0,0 +1,21 @@
+class TextGame
+  class << self
+    def render(key)
+      section = config[key] ? config[key] : config["default"]
+      message = section["message"]
+      choices = if section["choices"].nil?
+        "You have died... Thanks for playing!"
+      else
+        section["choices"].collect do |k, v|
+          "#{k} - #{v}"
+        end.join("\n")
+      end
+
+      [message, choices].join("\n")
+    end
+
+    def config
+      @config ||= YAML.load_file(Rails.root.join("config/game.yml"))
+    end
+  end
+end
